c83434266d1ef1e53052d79d5e4a4db4de6c356f
Ashesh Saha <78008554+hash-slash@users.noreply.github.com>
2022-01-05 16:07:50 +0530

    Update JULKA.py

--- JULKA.py
+++ JULKA.py
@@ -1,10 +1,9 @@
-// Problem: Julka
-// Contest: SPOJ - Classical
-// URL: https://www.spoj.com/problems/JULKA/
-// Memory Limit: 1536 MB
-// Time Limit: 2000 ms
-// 
-// Powered by CP Editor (https://cpeditor.org)
+# Problem: Julka
+# Contest: SPOJ - Classical
+# URL: https://www.spoj.com/problems/JULKA/
+# Memory Limit: 1536 MB
+# Time Limit: 2000 ms 
+# Powered by CP Editor (https://cpeditor.org)
 
 for i in range(10):
     G1 = int(input())
